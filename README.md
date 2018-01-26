146501bf3bd778ccb07bdc8c9b19d5107dfb0eff
AnatolyKoptelov <7497490@gmail.com>
2018-01-26 13:45:47 +0300

    Add new version of files

--- README.md
+++ README.md
@@ -1,6 +1,58 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+
+Application for counting words frequency on a text. File path of this text file is  command line required argument.
+By default, it works with encoded utf_8 files, but sometimes your files can have a different encoding. 
+You can use optional parameter **-c** for decode an original file. This application supports these codecs:
+
+ - utf_8
+ - cp1251
+ - koi8_r
+ - cp866,mac_cyrillic
+ 
+Use optional parameter **-q** for number of words setting on output
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.*:
+
+```
+$ python lang_frequency.py examles_of_texts/roman.txt
+
+the     11819
+of      7747
+and     3944
+to      3345
+in      2487
+a       1983
+was     1553
+by      1261
+that    1149
+his     1146
+```
+Use -h option for read application info:
+```
+$ python lang_frequency.py -h
+usage: lang_frequency.py [-h] [-q [QUANTITY]]
+                         [-c [{utf_8,cp1251,koi8_r,cp866,mac_cyrillic}]]
+                         path
+
+Frequency Analysis of Words
+
+positional arguments:
+  path                  File path
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -q [QUANTITY], --quantity [QUANTITY]
+                        How many frequent words to display
+  -c [{utf_8,cp1251,koi8_r,cp866,mac_cyrillic}], --codec [{utf_8,cp1251,koi8_r,cp866,mac_cyrillic}]
+                        Use for decode a original file
+```
+
+# Future
+
+In the next release will be add **-r** command line option for less frequent words displaying
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,88 @@
+import os
+import re
+import string
+import argparse
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_data(path, codec):
+    if os.path.isfile(path):
+        with open(path, 'r', encoding=codec) as file_to_read:
+            return file_to_read.read()
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text, quantity):
+    words = text.split(' ')
+    return Counter(words).most_common(quantity)
+
+
+def clear_text(text):
+    long_dash_utf8 = '—'
+    long_dash_cp1251 = '–'
+    for char in '{}{}{}'.format(
+        string.punctuation,
+        long_dash_utf8,
+        long_dash_cp1251,
+    ):
+        text = text.replace(char, ' ')
+    text = re.sub('\s+', ' ', text)
+    return text
+
+
+def get_args(list_of_allowable_codecs):
+   parser = argparse.ArgumentParser(
+        description='Frequency Analysis of Words'
+   )
+   parser.add_argument(
+        'path',
+        metavar='path',
+        type=str,
+        help='File path'
+   )
+   parser.add_argument(
+        '-q',
+        '--quantity',
+        action='store',
+        nargs='?',
+        type=int,
+        default=10,
+        help='How many frequent words to display'
+   )
+   parser.add_argument(
+        '-c',
+        '--codec',
+        action='store',
+        nargs='?',
+        default='utf_8',
+        choices=list_of_allowable_codecs,
+        help='Use for decode a original file',
+   )
+   return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    codecs = [
+        'utf_8',
+        'cp1251',
+        'koi8_r',
+        'cp866',
+        'mac_cyrillic',
+    ]
+    args = get_args(codecs)
+    try:
+        text = load_data(args.path, args.codec)
+    except UnicodeDecodeError as error:
+        print('{}\nCannot read file {} with {} codec\n{}'.format(
+            error,
+            args.path,
+            args.codec,
+            'Try to use other codec',
+        ))
+    else:
+        if text is not None:
+            text = clear_text(text)
+            most_frequent_words = get_most_frequent_words(text, args.quantity)
+            for word, entrances in most_frequent_words:
+                print('{}\t{}'.format(word, entrances))
+        else:
+            print('Cannot open "{}" '.format(args.path))
